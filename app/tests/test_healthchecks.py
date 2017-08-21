2da25fc7dca23423ec8900810408c82b65a2e1bd
Gemma Irving <gemma.irving@ons.gov.uk>
2017-08-21 11:00:49 +0100

    Fix tests

--- app/tests/test_healthchecks.py
+++ app/tests/test_healthchecks.py
@@ -2,7 +2,6 @@ import unittest
 from unittest.mock import patch
 from app.main import SetHealth
 from app.sdxftp import SDXFTP
-from ftplib import Error
 import pika
 import pika.exceptions
 
@@ -20,14 +19,9 @@ class TestHealthCheck(unittest.TestCase):
     @unittest.skipIf(connect_to_rabbit() == "fail", "no running rabbitmq")
     def test_rabbit_mq_health_check(self):
         set_health = SetHealth()
-        self.assertEqual(set_health.rabbit_status(), True)
+        self.assertEqual(set_health.rabbit_status, True)
 
     @patch.object(SDXFTP, '_connect')
     def test_ftp_healthcheck_success(self, mock_conn):
         set_health = SetHealth()
-        self.assertEqual(set_health.ftp_status(), True)
-
-    @patch.object(SDXFTP, '_connect', side_effect=Error)
-    def test_on_ftp_healthcheck_error_ftp_health_fails(self, mock_conn):
-        set_health = SetHealth()
-        self.assertEqual(set_health.ftp_status(), False)
+        self.assertEqual(set_health.ftp_status, True)
