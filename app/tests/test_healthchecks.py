955247352b624d664b56bc99a7763c8e45039b1e
Gemma Irving <gemma.irving@ons.gov.uk>
2017-08-21 13:16:11 +0100

    Modify skipif for healthcheck tests

--- app/tests/test_healthchecks.py
+++ app/tests/test_healthchecks.py
@@ -2,21 +2,24 @@ import unittest
 from unittest.mock import patch
 from app.main import SetHealth
 from app.sdxftp import SDXFTP
+from app import settings
+
 import pika
 import pika.exceptions
 
 
-def connect_to_rabbit():
+def rabbit_running():
     try:
-        pika.BlockingConnection(pika.URLParameters('amqp://admin:admin@0.0.0.0:5672'))
-        return "ok"
-    except pika.exceptions.ConnectionClosed:
-        return "fail"
+        connection = pika.BlockingConnection(pika.URLParameters(settings.RABBIT_URL))
+        connection.channel()
+        return True
+    except pika.exceptions.AMQPError:
+        return False
 
 
 class TestHealthCheck(unittest.TestCase):
 
-    @unittest.skipIf(connect_to_rabbit() == "fail", "no running rabbitmq")
+    @unittest.skipIf(not rabbit_running(), "This test requires a locally running rabbitmq")
     def test_rabbit_mq_health_check(self):
         set_health = SetHealth()
         self.assertEqual(set_health.rabbit_status, True)
