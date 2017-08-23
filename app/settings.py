41f299c4027aca6d998ebde06ee121428426b96e
Warren Bailey <warren@warrenbailey.net>
2017-08-23 13:04:22 +0100

    Namespace config

--- app/settings.py
+++ app/settings.py
@@ -31,7 +31,7 @@ FTP_USER = os.getenv('SEFT_FTP_USER', 'ons')
 FTP_PASS = os.getenv('SEFT_FTP_PASS', 'ons')
 FTP_FOLDER = os.getenv('SEFT_CONSUMER_FTP_FOLDER', '.')
 
-SDX_KEYS_FILE = os.getenv('SDX_KEYS_FILE', 'keys.yml')
+SDX_SEFT_CONSUMER_KEYS_FILE = os.getenv('SDX_SEFT_CONSUMER_KEYS_FILE', 'keys.yml')
 
 # Configure the number of retries attempted before failing call
 SERVICE_REQUEST_TOTAL_RETRIES = 5
