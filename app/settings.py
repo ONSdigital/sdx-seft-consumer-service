aa4eccfed131fcbe42ea9b0276e1223dae105731
Warren Bailey <warren@warrenbailey.net>
2017-08-30 11:01:46 +0100

    Fix merge issue, rename keys file (#19)

--- app/settings.py
+++ app/settings.py
@@ -42,7 +42,7 @@ FTP_USER = os.getenv('SEFT_FTP_USER', 'ons')
 FTP_PASS = os.getenv('SEFT_FTP_PASS', 'ons')
 FTP_FOLDER = os.getenv('SEFT_CONSUMER_FTP_FOLDER', '.')
 
-SDX_KEYS_FILE = os.getenv('SDX_KEYS_FILE', './sdx_test_keys/keys.yml')
+SDX_SEFT_CONSUMER_KEYS_FILE = os.getenv('SDX_SEFT_CONSUMER_KEYS_FILE', './sdx_test_keys/keys.yml')
 
 # Configure the number of retries attempted before failing call
 SERVICE_REQUEST_TOTAL_RETRIES = 5
