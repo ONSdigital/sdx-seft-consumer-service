09960a772dc73db6a1a15859c5a0a59b2bf9d791
Warren Bailey <warren@warrenbailey.net>
2018-04-30 16:16:54 +0100

    Code review comments

--- app/anti_virus_check.py
+++ app/anti_virus_check.py
@@ -1,7 +1,9 @@
 import collections
+import json
 import time
 
 import requests
+from requests.adapters import HTTPAdapter
 from sdc.rabbit.exceptions import QuarantinableError, RetryableError, BadMessageError
 
 from app import create_and_wrap_logger
@@ -15,12 +17,17 @@ AVResult = collections.namedtuple('AVResult', 'safe ready scan_results')
 class AntiVirusCheck:
     def __init__(self, tx_id):
         self.bound_logger = logger.bind(tx_id=tx_id)
+        self.session = requests.Session()
+        self.session.mount(settings.ANTI_VIRUS_BASE_URL, HTTPAdapter(max_retries=15))
 
     def send_for_av_scan(self, payload):
         self.bound_logger.info("Sending for AV check", filename=payload.file_name)
         data_id = self._send_for_anti_virus_check(payload.file_name, payload.decoded_contents)
         self.bound_logger.info("Sent for A/V check", data_id=data_id)
 
+        # this loop will block the consumer until the anti virus finishes
+        # in a future iteration we should make this asynchronous so that we
+        # can process multiple messages
         attempts = 0
         while attempts <= settings.ANTI_VIRUS_MAX_ATTEMPTS:
             attempts += 1
@@ -28,15 +35,13 @@ class AntiVirusCheck:
             if not results.ready:
                 time.sleep(settings.ANTI_VIRUS_WAIT_TIME)
             elif not results.safe:
-                error = "Unsafe file detected for case id {} with filename {}".format(payload.case_id,
-                                                                                      payload.file_name)
+                error = "Unsafe file detected".format(case_id=payload.case_id, filename=payload.file_name)
                 self._write_scan_report(results, payload.file_name)
                 self.bound_logger.error(error)
                 raise QuarantinableError()
             else:
                 self.bound_logger.debug(
-                    "File {} for case id {} has been virus checked and confirmed safe".format(payload.case_id,
-                                                                                              payload.file_name))
+                    "File has been virus checked and confirmed safe", case_id=payload.case_id, filename=payload.file_name)
                 return True
 
         # out of attempts raise retryable error to force the response back to the queue.
@@ -52,7 +57,10 @@ class AntiVirusCheck:
             headers['apikey'] = settings.ANTI_VIRUS_API_KEY
 
     def _check_av_response(self, response):
-        if response.status_code != 200:
+        try:
+            response.raise_for_status()
+        except requests.HTTPError as e:
+            self.bound_logger.error("Error received for A/V server", error=e)
             if response.status_code == 401:
                 self.bound_logger.critical("Invalid OPSWAT API Key - unable to continue")
                 raise RetryableError()
@@ -65,23 +73,29 @@ class AntiVirusCheck:
                 raise BadMessageError()
             elif 300 < response.status_code >= 500:
                 raise RetryableError()
-        else:
-            self.bound_logger.info("Response from AV OK")
 
     def _send_for_anti_virus_check(self, filename, contents):
-        url = settings.ANTI_VIRUS_BASE_URL + "file"
+        url = settings.ANTI_VIRUS_BASE_URL
         headers = {
             "filename": filename,
         }
         self._add_api_key(headers)
 
         self.bound_logger.info("Sending for A/V scan", url=url)
-        response = requests.post(url=url, headers=headers, data=contents)
+        try:
+            response = self.session.post(url=url, headers=headers, data=contents)
+        except requests.RequestException:
+            self.bound_logger.exception("Error sending request to Anti-virus server")
+            raise RetryableError()
 
         self._check_av_response(response)
 
-        self.bound_logger.info("Response received {}".format(response.text))
-        result = response.json()
+        self.bound_logger.info("Response received", response=response.text)
+        try:
+            result = response.json()
+        except json.JSONDecodeError():
+            self.bound_logger.exception("Unable to decode A/V results")
+            raise RetryableError()
 
         if result.get("err"):
             self.bound_logger.error("Unable to send file for anti virus scan: {}".format(result.get("err")))
@@ -95,42 +109,47 @@ class AntiVirusCheck:
             return data_id
 
     def _get_anti_virus_result(self, data_id):
-        url = settings.ANTI_VIRUS_BASE_URL + "file/" + data_id
+        url = settings.ANTI_VIRUS_BASE_URL + "/" + data_id
         headers = {}
         self._add_api_key(headers)
 
         self.bound_logger.info("Getting result for A/V scan", url=url)
 
-        response = requests.get(url=url, headers=headers)
+        try:
+            response = self.session.get(url=url, headers=headers)
+        except requests.RequestException:
+            self.bound_logger.exception("Error sending request to Anti-virus server")
+            raise RetryableError()
 
         self._check_av_response(response)
 
-        self.bound_logger.info("Response from AV {}".format(response.text))
+        self.bound_logger.info("Response from AV", response=response.text)
 
         result = response.json()
         scan_results = result.get("scan_results")
 
         ready = False
         safe = False
-
-        if scan_results:
-            progress_percentage = scan_results.get("progress_percentage")
-            if int(progress_percentage) == 100:
-                ready = True
-                self.bound_logger.info("Anti virus scan complete: {}".format(scan_results.get("scan_all_result_a")))
-                scan_all_result_i = scan_results.get("scan_all_result_i")
-                if int(scan_all_result_i) == 0:
-                    self.bound_logger.info("File is safe")
-                    safe = True
+        try:
+            if scan_results:
+                progress_percentage = scan_results.get("progress_percentage")
+                if int(progress_percentage) == 100:
+                    ready = True
+                    self.bound_logger.info("Anti virus scan complete", scan_results=scan_results.get("scan_all_result_a"))
+                    scan_all_result_i = scan_results.get("scan_all_result_i")
+                    if int(scan_all_result_i) == 0:
+                        self.bound_logger.info("File is safe")
+                        safe = True
+                    else:
+                        self.bound_logger.error("File is not safe")
                 else:
-                    self.bound_logger.error("File is not safe")
+                    self.bound_logger.info("Scan not yet complete")
             else:
-                self.bound_logger.info("Scan not yet complete")
-        else:
-            self.bound_logger.info("Results not yet available")
+                self.bound_logger.info("Results not yet available")
+        except TypeError:
+            self.bound_logger.exception("Unable to get progress percentage for A/V scan")
 
         return AVResult(safe=safe, ready=ready, scan_results=scan_results)
 
     def _write_scan_report(self, av_results, filename):
-        self.bound_logger.info("Writing scan report for file {}".format(filename))
-        self.bound_logger.error("A/V report", report=av_results.scan_results)
+        self.bound_logger.error("A/V report generated", filename=filename, report=av_results.scan_results)
--- app/tests/test_anti_virus_check.py
+++ app/tests/test_anti_virus_check.py
@@ -1,27 +1,21 @@
 import unittest
 
-import requests
-import requests_mock
+import responses
 from sdc.rabbit.exceptions import QuarantinableError, RetryableError, BadMessageError
 
 from app import settings
 from app.anti_virus_check import AntiVirusCheck
 from app.main import Payload
 
-session = requests.Session()
-adapter = requests_mock.Adapter()
-session.mount('mock', adapter)
-
 
 class AntiVirusCheckTests(unittest.TestCase):
-
-    @requests_mock.mock()
-    def test_send_for_av_scan_success(self, mock_request):
+    @responses.activate
+    def test_send_for_av_scan_success(self):
         data_id = '123'
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", json={'data_id': data_id})
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, json={'data_id': data_id}, status=200)
 
-        mock_request.get(settings.ANTI_VIRUS_BASE_URL + "file/" + data_id,
-                         json={'scan_results': {'progress_percentage': 100, 'scan_all_result_i': 0}})
+        responses.add(responses.GET, settings.ANTI_VIRUS_BASE_URL + "/" + data_id,
+                      json={'scan_results': {'progress_percentage': 100, 'scan_all_result_i': 0}}, status=200)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -29,13 +23,13 @@ class AntiVirusCheckTests(unittest.TestCase):
 
         self.assertTrue(anti_virus.send_for_av_scan(payload))
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_failure(self, mock_request):
+    @responses.activate
+    def test_send_for_av_scan_failure(self):
         data_id = '123'
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", json={'data_id': data_id})
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, json={'data_id': data_id}, status=200)
 
-        mock_request.get(settings.ANTI_VIRUS_BASE_URL + "file/" + data_id,
-                         json={'scan_results': {'progress_percentage': 100, 'scan_all_result_i': 1}})
+        responses.add(responses.GET, settings.ANTI_VIRUS_BASE_URL + "/" + data_id,
+                      json={'scan_results': {'progress_percentage': 100, 'scan_all_result_i': 1}}, status=200)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -44,14 +38,14 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(QuarantinableError):
             anti_virus.send_for_av_scan(payload)
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_not_ready_hits_max_attempts(self, mock_request):
+    @responses.activate
+    def test_send_for_av_scan_not_ready_hits_max_attempts(self):
         settings.ANTI_VIRUS_WAIT_TIME = 0.1
         data_id = '123'
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", json={'data_id': data_id})
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, json={'data_id': data_id}, status=200)
 
-        mock_request.get(settings.ANTI_VIRUS_BASE_URL + "file/" + data_id,
-                         json={'scan_results': {'progress_percentage': 50, 'scan_all_result_i': 0}})
+        responses.add(responses.GET, settings.ANTI_VIRUS_BASE_URL + "/" + data_id,
+                      json={'scan_results': {'progress_percentage': 50, 'scan_all_result_i': 0}}, status=200)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -60,9 +54,9 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(RetryableError):
             self.assertTrue(anti_virus.send_for_av_scan(payload))
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_forbidden_bad_api_key(self, mock_request):
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", status_code=401)
+    @responses.activate
+    def test_send_for_av_scan_forbidden_bad_api_key(self):
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, status=401)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -71,9 +65,9 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(RetryableError):
             anti_virus.send_for_av_scan(payload)
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_bad_request(self, mock_request):
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", status_code=400)
+    @responses.activate
+    def test_send_for_av_scan_bad_request(self):
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, status=400)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -82,9 +76,9 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(BadMessageError):
             anti_virus.send_for_av_scan(payload)
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_not_found(self, mock_request):
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", status_code=404)
+    @responses.activate
+    def test_send_for_av_scan_not_found(self):
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, status=404)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -93,9 +87,9 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(RetryableError):
             anti_virus.send_for_av_scan(payload)
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_forbidden(self, mock_request):
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", status_code=403)
+    @responses.activate
+    def test_send_for_av_scan_forbidden(self):
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, status=403)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -104,9 +98,9 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(BadMessageError):
             anti_virus.send_for_av_scan(payload)
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_internal_server_error(self, mock_request):
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", status_code=500)
+    @responses.activate
+    def test_send_for_av_scan_internal_server_error(self):
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, status=500)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
@@ -115,9 +109,9 @@ class AntiVirusCheckTests(unittest.TestCase):
         with self.assertRaises(RetryableError):
             anti_virus.send_for_av_scan(payload)
 
-    @requests_mock.mock()
-    def test_send_for_av_scan_service_unavailable(self, mock_request):
-        mock_request.post(settings.ANTI_VIRUS_BASE_URL + "file", status_code=503)
+    @responses.activate
+    def test_send_for_av_scan_service_unavailable(self):
+        responses.add(responses.POST, settings.ANTI_VIRUS_BASE_URL, status=503)
 
         anti_virus = AntiVirusCheck(tx_id=1)
 
