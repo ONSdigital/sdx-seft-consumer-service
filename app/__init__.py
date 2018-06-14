20a98bb984de682e8bf68ddf35fd8236c5dc2e55
Warren Bailey <warren@warrenbailey.net>
2018-06-14 10:41:58 +0100

    Release 2.0.0

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import app.settings
 from structlog import wrap_logger
 
 
-__version__ = "1.4.0"
+__version__ = "2.0.0"
 __service__ = "sdx-seft-consumer-service"
 
 logging.basicConfig(format=app.settings.LOGGING_FORMAT,
